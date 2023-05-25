cf450f9a5cbaa1ca4c8ccd983e2818ed722f9d2d
deniumden <deniumden@gmail.com>
2023-05-25 16:01:20 +0300

    25.05.2023

--- Tasks/scriptTasks.js
+++ Tasks/scriptTasks.js
@@ -378,3 +378,299 @@ function fib(fibNum) {
   return `0 1 ${strFib}`;
 }
 console.log(fib(1));
+
+// Coding Exercise 10: Задачи на работу с объектами
+// ---------------------------------------------
+
+// №1
+// Напишите функцию showExperience, которая будет принимать в
+// себя объект со всеми данными и возвращать строку с опытом.
+
+// Пример:
+
+// showExperience(personalPlanPeter) => '1 month'
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+};
+
+function showExperience(personalPlanPeter) {
+  const { exp } = personalPlanPeter.skills;
+  return exp;
+}
+console.log(showExperience(personalPlanPeter));
+
+// №2
+// Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми
+// данными и возвращать строку в нужном виде.
+
+// Пример:
+
+// showProgrammingLangs(personalPlanPeter)  =>
+
+// "Язык js изучен на 20% Язык php изучен на 10%"
+
+// Причем функция должна работать вне зависимости от количества языков.
+// Если ни один не указан, то возвращается пустая строка.
+// P.S. Для переноса строки используется \n в конце строки.
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+};
+
+function showProgrammingLangs(personalPlanPeter) {
+  let str = "";
+  const { programmingLangs } = personalPlanPeter.skills;
+  for (let key in programmingLangs) {
+    str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+  }
+
+  return str;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+console.log(showExperience(personalPlanPeter));
+
+// Создайте метод showAgeAndLangs внутри объекта personalPlanPeter.
+// При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+// Пример:
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в
+// верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+  showAgeAndLangs: function (personalPlanPeter) {
+    let str = ``;
+    const { age } = personalPlanPeter;
+    const { languages } = personalPlanPeter.skills;
+    str = `Мне ${age} и я владею языками: `;
+
+    languages.forEach(function (lang) {
+      str += `${lang.toUpperCase()} `;
+    });
+
+    return str;
+  },
+};
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+// Coding Exercise 11+12: Задачи на работу с массивами
+// ---------------------------------------------
+// №1
+// Напишите функцию showFamily, которая будет принимать в себя массив строк и
+// возвращать сообщение в нужном формате.
+
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+
+// Имена подставляются автоматически из массива. Если массив пустой, то
+// выводится сообщение 'Семья пуста'
+
+const family = [`Peter`, `Ann`, `Alex`, `Linda`];
+
+function showFamily(family) {
+  let str = ``;
+  if (family.length == 0) {
+    str += `Семья пуста`;
+  } else {
+    str += `Семья состоит из: ${family.join(`, `)}`;
+    return str;
+  }
+}
+
+console.log(showFamily(family));
+
+// #2
+// напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет
+// выводить в консоль эти строки в нижнем регистре.
+
+// Пример:
+
+// standardizeStrings(favoriteCities)  выведет в консоль
+
+// lisbon
+// rome
+// milan
+// dublin
+// Это частая задача в реальности, так как от пользователя нам могут прийти ответы в самых
+// разных форматах. В том числе и с разными буквами :) Поэтому нам нужно привести строки в
+// один формат для правильной работы.
+
+const favoriteCities = [`Lisbon`, `rOme`, `MILAN`, `duBLin`];
+
+function standardizeStrings(favoriteCities) {
+  let str = ``;
+  for (let key in favoriteCities) {
+    str += `${favoriteCities[key].toLowerCase()}\n`;
+  }
+  return str;
+}
+
+console.log(standardizeStrings(favoriteCities));
+
+// Решение
+
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+  arr.forEach((city) => {
+    console.log(city.toLowerCase());
+  });
+}
+
+standardizeStrings(favoriteCities);
+
+// #3
+// Задача с собеседований. Напишите функцию reverse,
+// которая принимает в себя строку и возвращает эту строку в обратном порядке.
+
+// Пример:
+
+// const someString = 'This is some strange string';
+// reverse(someString) => 'gnirts egnarts emos si sihT'
+
+// Функцию можно применить к любой строке.
+// Если в функцию приходит не строка - вернуть сообщение "Ошибка!"
+
+const someString = "This is some strange string";
+
+function reverse(someString) {
+  return someString.split(``).reverse().join(``);
+}
+
+console.log(reverse(someString));
+
+// Решение
+
+const someString = "This is some strange string";
+
+function reverse(str) {
+  if (typeof str !== "string") {
+    return "Ошибка!";
+  }
+  // Самый оптимальный вариант решения
+  return str.split("").reverse().join("");
+
+  // Решение при помощи цикла
+  // let newStr = '';
+  // for (let i = str.length - 1; i >= 0; i--) {
+  //     newStr += str[i];
+  // }
+  // return newStr
+}
+
+console.log(reverse(someString));
+
+// №4
+//  Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги
+// из двух разных банков в разных валютах. Один банк основной с базовыми валютами,
+// второй дополнительный с прочими валютами:
+
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// Вам нужно создать главную функцию банкомата availableCurr, которая принимает два
+// аргумента: первый - это массив со всеми доступными валютами из двух банков сразу
+// (сейчас представим, что они не могут повторяться), второй - необязательный аргумент,
+// который указывает ту валюту, которая сейчас закончилась в банкомате.
+// Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'.
+// Функция возвращает строку в нужном виде.
+
+// Пример:
+
+// availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+// Вернет строку:
+
+// Доступные валюты:
+// UAH
+// RUB
+// Заметьте:
+
+// - CNY (юань) исчез из списка валют, значит такая валюта закончилась
+// - После валюты: стоит перенос строки \n, и после каждой валюты тоже.
+// Это важно для тестов
+// - Данные для первого аргумента должны приходить сразу из двух банков,
+// причем сначала baseCurrencies, потом additionalCurrencies по порядку
+
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+const allCurr = [...baseCurrencies, ...additionalCurrencies];
+// const nilCurr = ["UAH"];
+
+function availableCurr(allCurr, nilCurr) {
+  let str = "";
+  if (allCurr.length === 0) {
+    str = `Нет доступных валют`;
+  } else {
+    for (let key in allCurr) {
+      if (allCurr[key] == nilCurr) {
+        continue;
+      } else {
+        // str += `\n`;
+        str += `\n${allCurr[key]}`;
+      }
+    }
+  }
+
+  return str;
+}
+
+console.log(availableCurr(allCurr, `UAH`));
+
+// Решение
+
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+function availableCurr(arr, missingCurr) {
+  let str = "";
+  arr.length === 0
+    ? (str = "Нет доступных валют")
+    : (str = "Доступные валюты:\n");
+
+  arr.forEach(function (curr, i) {
+    if (curr !== missingCurr) {
+      str += `${curr}\n`;
+    }
+  });
+
+  // Или
+  // for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] === missingCurr) {
+  //         continue;
+  //     }
+  //     str += `${arr[i]}\n`;
+  // }
+
+  return str;
+}
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], "CNY");
--- Tutor Field/tutorField.js
+++ Tutor Field/tutorField.js
@@ -57,7 +57,7 @@ newNumbers.a = 11;
 newNumbers.c.x = 11;
 
 console.log(newNumbers);
-console.log(numbers);
+console.log(numbers.b);
 
 // [Running] node "c:\Users\PAVILION\Documents\TUTOR\JavaScript\Tutor Field\tempCodeRunnerFile.js"
 // { a: 11, b: 5, c: { x: 11, y: 4 } }
@@ -126,3 +126,106 @@ const q = {
 
 const newObj = { ...q };
 console.log(newObj);
+
+// Задачи на работу с объектами
+// №1
+// Напишите функцию showExperience, которая будет принимать в
+// себя объект со всеми данными и возвращать строку с опытом.
+
+// Пример:
+
+// showExperience(personalPlanPeter) => '1 month'
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+};
+
+function showExperience(personalPlanPeter) {
+  const { exp } = personalPlanPeter.skills;
+  return exp;
+}
+console.log(showExperience(personalPlanPeter));
+
+// №2
+// Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми
+// данными и возвращать строку в нужном виде.
+
+// Пример:
+
+// showProgrammingLangs(personalPlanPeter)  =>
+
+// "Язык js изучен на 20% Язык php изучен на 10%"
+
+// Причем функция должна работать вне зависимости от количества языков.
+// Если ни один не указан, то возвращается пустая строка.
+// P.S. Для переноса строки используется \n в конце строки.
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+};
+
+function showProgrammingLangs(personalPlanPeter) {
+  let str = "";
+  const { programmingLangs } = personalPlanPeter.skills;
+  for (let key in programmingLangs) {
+    str += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+  }
+
+  return str;
+}
+
+console.log(showProgrammingLangs(personalPlanPeter));
+
+console.log(showExperience(personalPlanPeter));
+
+// Создайте метод showAgeAndLangs внутри объекта personalPlanPeter.
+// При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+// Пример:
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в
+// верхнем регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+    languages: ["ru", "eng"],
+    programmingLangs: {
+      js: "20%",
+      php: "10%",
+    },
+    exp: "1 month",
+  },
+  showAgeAndLangs: function (personalPlanPeter) {
+    let str = ``;
+    const { age } = personalPlanPeter;
+    const { languages } = personalPlanPeter.skills;
+    str = `Мне ${age} и я владею языками: `;
+
+    languages.forEach(function (lang) {
+      str += `${lang.toUpperCase()} `;
+    });
+
+    return str;
+  },
+};
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
